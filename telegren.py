368e5d40073dd0fb846c71019daf3e30e4be2a08
mkmozgawa <mzgw@protonmail.com>
2019-05-02 21:21:59 +0200

    Initial commit

---
+++ telegren.py
@@ -0,0 +1,40 @@
+import telegram
+import json
+import time
+
+with open('token.txt', 'r') as t:
+    TOKEN = t.read().strip()
+
+with open('reply.txt', 'r') as r:
+    REPLY = r.read().strip()
+
+with open('keyword.txt', 'r') as k:
+    KEYWORD = k.read().strip()
+
+with open('offset.txt', 'r') as o:
+    OFFSET = int(o.read().strip())
+
+bot = telegram.Bot(token=TOKEN)
+
+while True:
+    try:
+        updates = bot.get_updates(offset=OFFSET)
+    except telegram.error.TimedOut:
+        continue
+
+    if len(updates) > 0:
+        chat_id = updates[-1].message.chat_id
+        OFFSET = updates[-1].update_id + 1
+
+        for update in updates:
+            message = update.message.text
+            if KEYWORD in message.lower():
+                bot.send_message(chat_id=chat_id, text=REPLY)
+                break
+
+        with open('offset.txt', 'w') as o:
+            o.write(str(OFFSET))
+        
+    time.sleep(1)
+
+    
