34abf46a6229acd2192e81f13210b7509d6d76b6
mkmozgawa <mzgw@protonmail.com>
2019-05-03 17:23:17 +0200

    Add the functions create_bot and fetch_updates

--- telegren.py
+++ telegren.py
@@ -1,8 +1,28 @@
 import telegram
 import json
 import time
+import sys
 
 from setup import create_file, read_file, TOKEN_FILE, KEYWORD_FILE, REPLY_FILE, OFFSET_FILE
+
+def create_bot(token):
+    ''' Return a bot instance. '''
+    try:
+        return telegram.Bot(token=token)
+    except telegram.error.InvalidToken:
+        print("Invalid token. I know you've tried your best, though.")
+        sys.exit(1)
+
+def fetch_updates(bot, offset):
+    ''' Return the newest updates (based off the offset value) if possible. If there are new updates return None. '''
+    try:
+        updates = bot.get_updates(offset=offset)
+        if len(updates) > 0:
+            return updates
+        else:
+            return None
+    except telegram.error.TimedOut:
+        return None
 
 def run_bot():
 
@@ -11,15 +31,11 @@ def run_bot():
     REPLY = read_file(REPLY_FILE)
     OFFSET = read_file(OFFSET_FILE)
 
-    bot = telegram.Bot(token=TOKEN)
+    bot = create_bot(TOKEN)
 
     while True:
-        try:
-            updates = bot.get_updates(offset=OFFSET)
-        except telegram.error.TimedOut:
-            continue
-
-        if len(updates) > 0:
+        updates = fetch_updates(bot, OFFSET)
+        if updates is not None:
             chat_id = updates[-1].message.chat_id
             OFFSET = updates[-1].update_id + 1
 
