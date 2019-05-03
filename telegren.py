bb8e890e8551efd7b63c4d6f94a1000dae6d8ed3
mkmozgawa <mzgw@protonmail.com>
2019-05-03 13:57:09 +0200

    Create one big-ass function in telegren to cover the bot

--- telegren.py
+++ telegren.py
@@ -2,41 +2,46 @@ import telegram
 import json
 import time
 
-with open('token.txt', 'r') as t:
-    TOKEN = t.read().strip()
+def run_bot():
 
-with open('reply.txt', 'r') as r:
-    REPLY = r.read().strip()
+    with open('token.txt', 'r') as t:
+        TOKEN = t.read().strip()
 
-with open('keyword.txt', 'r') as k:
-    KEYWORD = k.read().strip()
+    with open('reply.txt', 'r') as r:
+        REPLY = r.read().strip()
 
-with open('offset.txt', 'r') as o:
-    OFFSET = int(o.read().strip())
+    with open('keyword.txt', 'r') as k:
+        KEYWORD = k.read().strip()
 
-bot = telegram.Bot(token=TOKEN)
+    with open('offset.txt', 'r') as o:
+        OFFSET = int(o.read().strip())
 
-while True:
-    try:
-        updates = bot.get_updates(offset=OFFSET)
-    except telegram.error.TimedOut:
-        continue
+    bot = telegram.Bot(token=TOKEN)
 
-    if len(updates) > 0:
-        chat_id = updates[-1].message.chat_id
-        OFFSET = updates[-1].update_id + 1
+    while True:
+        try:
+            updates = bot.get_updates(offset=OFFSET)
+        except telegram.error.TimedOut:
+            continue
 
-        for update in updates:
-            try:
-                message = update.message.text
-                if message is not None and KEYWORD in message.lower():
-                    bot.send_message(chat_id=chat_id, text=REPLY)
-                    break
-            except AttributeError:
-                continue
+        if len(updates) > 0:
+            chat_id = updates[-1].message.chat_id
+            OFFSET = updates[-1].update_id + 1
 
-        with open('offset.txt', 'w') as o:
-            o.write(str(OFFSET))
+            for update in updates:
+                try:
+                    message = update.message.text
+                    if message is not None and KEYWORD in message.lower():
+                        bot.send_message(chat_id=chat_id, text=REPLY)
+                        break
+                except AttributeError:
+                    continue
+
+            with open('offset.txt', 'w') as o:
+                o.write(str(OFFSET))
         
-    time.sleep(1)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    run_bot()
     
