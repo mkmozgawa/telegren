cf6d47c419512dbfe398b6279fc174d4ebad1296
mkmozgawa <mzgw@protonmail.com>
2019-05-04 20:01:00 +0200

    bug fix

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ def process_updates(updates, bot, KEYWORD, REPLY):
     ''' Process the updates. '''
     for update in updates:
         try:
-            match = find_matches(update.message.text)
+            match = find_matches(update.message.text, KEYWORD)
             if match is not None:
                 respond_to_message(update, match, REPLY)
         except AttributeError: # raised when it can't process a message (sticker, image, etc)
@@ -52,11 +52,11 @@ def get_offset(updates):
     ''' Return the offset after processing the most recent messages. +1 so the next query only asks for the messages that haven't been processed yet. '''
     return updates[-1].update_id + 1
 
-def find_matches(text):
+def find_matches(text, keyword):
     ''' Find the matching elements in the text, if any, and return them as a string of quotes with question marks. '''
     try:
         arr = text.split(' ')
-        matches = ['"' + el + '"?' for el in arr if KEYWORD in el.lower()]
+        matches = ['"' + el + '"?' for el in arr if keyword in el.lower()]
         if len(matches) > 0:
             return ' '.join(matches)
         else:
